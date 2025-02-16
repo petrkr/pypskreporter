3d7b67242608256624e9300238cdcb957c530b5a
Petr Kracik <petrkr@petrkr.net>
2025-02-16 01:51:46 +0100

    Test with server info

--- test.py
+++ test.py
@@ -7,9 +7,13 @@ def main():
     remote = RemoteInfo("OK1PKR-REMOTE", 14095, "FREEDV", -3, "JO70FC")
     local = LocalInfo("OK1PKR-LOCAL", "JO70FC", "PyPSKReporter", "0.0.1", "35.89421911 139.94637467", "Antenna in trees")
 
-    pskreporter = PSKReporter()
-    pskreporter.reporter_seen_callsign(remote, local)
+    # Use development port
+    # Check https://pskreporter.info/cgi-bin/psk-analysis.pl for reports
+    pskreporter = PSKReporter("report.pskreporter.info", 14739)
 
+    print(pskreporter)
+
+    pskreporter.reporter_seen_callsign(remote, local)
 
 
 if __name__ == "__main__":
