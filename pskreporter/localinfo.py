158ca62f825a6aead3c393927cc610c5cf726e7c
Petr Kracik <petrkr@petrkr.net>
2025-02-16 01:10:19 +0100

    LocalInfo: use LonLat for grid as PSKReporter.dll does

--- pskreporter/localinfo.py
+++ pskreporter/localinfo.py
@@ -1,8 +1,14 @@
 class LocalInfo:
-    def __init__(self, callsign, grid, latlng, program_id, program_version, antenna = None):
+    def __init__(self, callsign, grid, program_id, program_version, latlng = None, antenna = None):
         self._callsign = callsign
         self._grid = grid
         self._latlng = latlng
+
+        # If latlng exists, override grid
+        grid_lonlat = self._get_grid_from_latlng()
+        if grid_lonlat:
+            self._grid = grid_lonlat
+
         self._program_id = program_id
         self._program_version = program_version
         self._antenna = antenna
@@ -16,6 +22,13 @@ class LocalInfo:
         return self.__str__()
 
 
+    def _get_grid_from_latlng(self):
+        if not self._latlng:
+            return None
+
+        # TODO: Implement this, for now return None
+        return None
+
     @property
     def callsign(self):
         return self._callsign
@@ -23,10 +36,6 @@ class LocalInfo:
     @property
     def grid(self):
         return self._grid
-
-    @property
-    def latlng(self):
-        return self._latlng
 
     @property
     def program_id(self):
@@ -45,7 +54,6 @@ class LocalInfo:
         return {
             "callsign": self._callsign,
             "grid": self._grid,
-            "latlng": self._latlng,
             "program_id": self._program_id,
             "program_version": self._program_version,
             "antenna": self._antenna
--- test.py
+++ test.py
@@ -5,10 +5,11 @@ from pskreporter.localinfo import LocalInfo
 
 def main():
     remote = RemoteInfo("OK1PKR-REMOTE", 14095, "FREEDV", -3, "JO70FC")
-    local = LocalInfo("OK1PKR-LOCAL", "JO70FC", "35.89421911 139.94637467", "PyPSKReporter", "0.0.1")
+    local = LocalInfo("OK1PKR-LOCAL", "JO70FC", "PyPSKReporter", "0.0.1", "35.89421911 139.94637467", "Antenna in trees")
 
-    print("Remote: ", remote)
-    print("Local: ", local)
+    pskreporter = PSKReporter()
+    pskreporter.reporter_seen_callsign(remote, local)
+
 
 
 if __name__ == "__main__":
